800f646c613b5b753a7aaf5bb9935a1211158135
Emerson-lab <trinddade29@gmail.com>
2021-10-30 21:07:01 -0300

    add analyze bundle

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultProps {
@@ -9,23 +10,37 @@ interface SearchResultProps {
     priceFormatted: string;
     title: string;
   }>;
-  onAddToWishList: (id: number) => void
+  onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({totalPrice, results, onAddToWishList }: SearchResultProps) {
- 
+export function SearchResults({
+  totalPrice,
+  results,
+  onAddToWishList,
+}: SearchResultProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}R$</h2>
-      {results.map((product) => {
-        return (
-          <ProductItem 
-            key={product.id}
-            product={product} 
-            onAddToWishList={onAddToWishList}
-          />
-        );
-      })}
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
